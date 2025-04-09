70747d5c9c80704d1aabac0fcc7682371373afb0
Benfranklin m s <bfranklinms@gmail.com>
2025-04-09 08:36:48 +0530

    feat: add scatterplot script for car age and speed analysis

---
+++ Exp 16 - Scatterplot.R
@@ -0,0 +1,4 @@
+age <- c(1, 2, 5, 10, 30, 12, 24, 14, 15, 17, 12, 23, 2, 5)
+speed <- c(34, 120, 340, 240, 100, 120, 100, 20, 34, 45, 60, 70, 70, 140)
+
+plot(age, speed, main = "Observation of cars", xlab = "Car age", ylab = "Car speed")

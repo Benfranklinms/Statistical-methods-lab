c11a55908f7411f4bcfef6297358bde950075414
Benfranklin m s <bfranklinms@gmail.com>
2025-04-09 09:19:15 +0530

    feat: add histogram script for student height distribution

---
+++ Exp 20 - Histogram.R
@@ -0,0 +1,4 @@
+data <- c(rep(137.5, 4), rep(142.5, 12), rep(147.5, 16), rep(152.5, 8))
+breaks <- c(135, 140, 145, 150, 155)
+
+hist(data, breaks = breaks, main = "Height of students", xlab = "Height", ylab = "No. of students", col = "Turquoise")
